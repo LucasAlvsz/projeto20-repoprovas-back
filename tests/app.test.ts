e9cdc7b5122da1ac47dc9a527beacfa51c43a6d5
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-07-21 16:51:43 -0300

    chore: signup test

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,7 +1,23 @@
+import prisma from "@/db"
 import app from "@/index"
 import supertest from "supertest"
 
-it("Should return status code 201", () => {
-	const user: string = "user"
-	expect(user).toBe("user")
+beforeEach(async () => {
+	await prisma.$executeRaw`DELETE FROM users WHERE email = 'lucax@gmail.com'`
+})
+
+describe("SignUp", () => {
+	it("Should return status code 201", async () => {
+		const login = {
+			name: "lucax",
+			email: "lucax@gmail.com",
+			password: "lucax",
+		}
+		const response = await supertest(app).post("/SignUp").send(login)
+		expect(response.status).toEqual(201)
+	})
+})
+
+afterAll(async () => {
+	await prisma.$disconnect()
 })
